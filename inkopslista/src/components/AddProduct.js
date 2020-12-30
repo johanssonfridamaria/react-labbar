62426c083ba7499551298f58108006168cf6e265
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 14:45:15 +0100

    created addproduct component

---
+++ inkopslista/src/components/AddProduct.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const AddProduct = () => {
+  return (
+    <div className="addProduct">
+      <input type="text"/>
+      <button>Lägg till</button>
+    </div>
+  )
+}
+
+export default AddProduct
