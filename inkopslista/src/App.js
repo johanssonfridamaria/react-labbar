6a9310f44f85dcf7a5991e8e51184ed41b06dc30
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 17:45:56 +0100

    removed unused comp

--- inkopslista/src/App.js
+++ inkopslista/src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './App.css';
 import AddProduct from './components/AddProduct';
-import BoughtProducts from './components/BoughtProducts';
 import ShoppingList from './components/ShoppingList';
 
 function App() {
@@ -35,24 +34,60 @@ function App() {
     },
   ])
 
+  // const [doneProducts, setDoneProducts] = useState([])
+
   const setProductsCallback = productName => {
+    console.log(productName)
     setProducts(prevProducts => {
       return [
         { id: uuidv4(), name: productName, completed: false },
         ...prevProducts
       ]
     })
-
   }
 
+  const removeProductCallback = id => {
+    setProducts(prevProducts => prevProducts.filter(product => product.id !== id))
+  }
+
+  const editProductCallback = (id, newName) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(product => {
+        if (product.id === id) {
+          product.name = newName
+          return product
+        }
+        return product
+      })
+    })
+  }
+
+  const toogleCompleteCallback = id => {
+    let newProducts = products.map(product => {
+      if (product.id === id) {
+        product.completed = !product.completed
+        return product
+      }
+      return product
+    })
+    setProducts(newProducts)
+  }
+
+  // const setDoneProducts = 
 
   return (
     <div className="app">
       <div className="card">
         <h1>Inköpslista</h1>
         <AddProduct setProductsCallback={setProductsCallback} />
-        <ShoppingList products={products} />
-        <BoughtProducts products={products} />
+        <div className="list">
+          <h2>Att köpa:</h2>
+          <ShoppingList products={products.filter(product => !product.completed)} removeProductCallback={removeProductCallback} editProductCallback={editProductCallback} toogleCompleteCallback={toogleCompleteCallback} />
+        </div>
+        <div className="list">
+          <h2>I varukorgen</h2>
+          <ShoppingList products={products.filter(product => product.completed)} removeProductCallback={removeProductCallback} editProductCallback={editProductCallback} toogleCompleteCallback={toogleCompleteCallback} />
+        </div>
       </div>
     </div>
   );
