24ae983c5a8811b01eb9312ec09618dc065ade8d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 16:47:01 +0100

    changed class to className

--- inkopslista/src/components/Product/Product.js
+++ inkopslista/src/components/Product/Product.js
@@ -6,13 +6,13 @@ const Product = ({ product }) => {
   return (
     <div className="product">
       <div className="product-name">
-        <button className="btn-product"><i class="fas fa-check"></i></button>
+        <button className="btn-product"><i className="fas fa-check"></i></button>
         <ProductName product={product} />
-        {/* <ProductEdit product={product} /> */}
+        <ProductEdit product={product} />
       </div>
       <div>
-        <button className="btn-product btn-edit"><i class="fas fa-pen"></i></button>
-        <button className="btn-product btn-delete"><i class="fas fa-minus"></i></button>
+        <button className="btn-product btn-edit"><i className="fas fa-pen"></i></button>
+        <button className="btn-product btn-delete"><i className="fas fa-minus"></i></button>
       </div>
     </div>
   )
