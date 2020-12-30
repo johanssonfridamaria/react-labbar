de30e19be55ad4d2876d7b99b5eec8b71c601280
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 16:25:57 +0100

    added icons on buttons and added styling classes

--- inkopslista/src/components/Product/Product.js
+++ inkopslista/src/components/Product/Product.js
@@ -5,12 +5,14 @@ import ProductEdit from './ProductEdit'
 const Product = ({ product }) => {
   return (
     <div className="product">
-      <button><i class="fas fa-check"></i></button>
-      <ProductName product={product} />
-      <ProductEdit product={product} />
+      <div className="product-name">
+        <button className="btn-product"><i class="fas fa-check"></i></button>
+        <ProductName product={product} />
+        {/* <ProductEdit product={product} /> */}
+      </div>
       <div>
-        <button>edit</button>
-        <button>delete</button>
+        <button className="btn-product btn-edit"><i class="fas fa-pen"></i></button>
+        <button className="btn-product btn-delete"><i class="fas fa-minus"></i></button>
       </div>
     </div>
   )
