ff4c10161e27f10ed0deb5560f1cd4e484e2b4c4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 17:47:14 +0100

    added methods

--- inkopslista/src/components/Product/ProductEdit.js
+++ inkopslista/src/components/Product/ProductEdit.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 
-const ProductEdit = () => {
+const ProductEdit = ({product, setEdit, editProductCallback}) => {
+
+  const input = useRef(); 
+
+  useEffect(()=>{
+    input.current.value = product.name
+  }, [product.name])
+
+  const editProduct = (e) => {
+    e.preventDefault();
+    editProductCallback(product.id, input.current.value);
+    setEdit(false);
+  }
+
   return (
     <div>
-      <input type="text"/>
-      <button>OK</button>
+      <input type="text" ref={input}/>
+      <button onClick={editProduct}>OK</button>
     </div>
   )
 }
