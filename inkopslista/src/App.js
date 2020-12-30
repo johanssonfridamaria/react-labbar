d628bb856f7b603afdce4d842c74ab887629a8da
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 14:53:04 +0100

    added products array

--- inkopslista/src/App.js
+++ inkopslista/src/App.js
@@ -1,15 +1,47 @@
+import {useState} from 'react'
+import {v4 as uuidv4 } from 'uuid'
 import './App.css';
 import AddProduct from './components/AddProduct';
 import BoughtProducts from './components/BoughtProducts';
 import ShoppingList from './components/ShoppingList';
 
 function App() {
+
+  const [products, setproducts] = useState([
+    {
+      id: uuidv4(),
+      name: 'Chips',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'Smördeg',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'Pesto',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'Fläskfilé',
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'Bearnaise',
+      completed: false,
+    },
+  ])
+
+
   return (
     <div className="app">
       <div className="card">
         <h1>Inköpslista</h1>
         <AddProduct />
-        <ShoppingList />
+        <ShoppingList products={products}/>
         <BoughtProducts />
       </div>
     </div>
