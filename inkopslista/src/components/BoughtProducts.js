4996d45846adb91653d278ecbdd69170bf847800
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 14:45:36 +0100

    created components and added headlines

---
+++ inkopslista/src/components/BoughtProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoughtProducts = () => {
+  return (
+    <div>
+      <h2>I varukorgen</h2>
+    </div>
+  )
+}
+
+export default BoughtProducts
---
+++ inkopslista/src/components/ShoppingList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoppingList = () => {
+  return (
+    <div>
+      <h2>Att köpa:</h2>
+    </div>
+  )
+}
+
+export default ShoppingList
