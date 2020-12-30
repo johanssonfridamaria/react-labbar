d166d443a5cb1ec197c7e6206154b306caf05732
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 14:45:01 +0100

    imported components

--- inkopslista/src/App.js
+++ inkopslista/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import AddProduct from './components/AddProduct';
+import BoughtProducts from './components/BoughtProducts';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="app">
-      
+      <div className="card">
+        <h1>Inköpslista</h1>
+        <AddProduct />
+        <ShoppingList />
+        <BoughtProducts />
+      </div>
     </div>
   );
 }
