6dc6b4ee042701492a555ef97005eab03c9a13a2
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-12-30 17:47:03 +0100

    added methods

--- inkopslista/src/components/Product/Product.js
+++ inkopslista/src/components/Product/Product.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProductName from './ProductName'
 import ProductEdit from './ProductEdit'
 
-const Product = ({ product }) => {
+const Product = ({ product, removeProductCallback, editProductCallback, toogleCompleteCallback }) => {
+
+  const [edit, setEdit] = useState(false)
+
+  const toggleEdit = () => {
+    setEdit(!edit)
+  }
+
+
   return (
     <div className="product">
       <div className="product-name">
-        <button className="btn-product"><i className="fas fa-check"></i></button>
-        <ProductName product={product} />
-        <ProductEdit product={product} />
+        <button className={`btn-product ${product.completed ? "btn-done" : "btn-not-done"}`}onClick={()=>toogleCompleteCallback(product.id)}><i className="fas fa-check"></i></button>
+        { edit
+        ? <ProductEdit product={product} setEdit={setEdit} editProductCallback={editProductCallback}/>
+        : <ProductName product={product} />
+        }
       </div>
       <div>
-        <button className="btn-product btn-edit"><i className="fas fa-pen"></i></button>
-        <button className="btn-product btn-delete"><i className="fas fa-minus"></i></button>
+        <button className="btn-product btn-edit" onClick={toggleEdit}><i className="fas fa-pen"></i></button>
+        <button className="btn-product btn-delete" onClick={()=>removeProductCallback(product.id)}><i className="fas fa-minus"></i></button>
       </div>
     </div>
   )
