98470f8846b72d30ca6ae30b015c3c08f264cdea
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 00:45:50 +0100

    created post card component

---
+++ blog-app/src/components/PostCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles'
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 250,
+    // height: -webkit-fill-available
+  },
+})
+
+const PostCard = ({ post }) => {
+
+  const classes = useStyles();
+  const truncate = (str) => {
+    return str.length > 10 ? str.substring(0, 50) + "..." : str;
+  }
+
+  return (
+    <Card className={classes.root}>
+      <CardContent >
+        <Typography variant="h5" gutterBottom align="center">
+          {post.title}
+        </Typography>
+        <Typography variant="body2">
+          {truncate(post.body)}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button>Read more</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default PostCard
