24a29d0745c22a73181ea556c3681ec529d90d6f
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 00:44:30 +0100

    imported components and created routes. Added getPost method and fetched from api

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,9 +1,40 @@
+import { useState, useEffect } from 'react';
+// import {v4 as uuidv4 } from 'uuid'
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Container } from '@material-ui/core';
+import CreatePost from './views/CreatePost';
+import Posts from './views/Posts';
+import axios from 'axios';
 
 function App() {
+
+  const [posts, setposts] = useState([]);
+  const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+  const getPosts = async () => {
+    const response = await axios.get(url)
+    setposts(response.data)
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Container fixed mt="2rem">
+        <Switch>
+          <Route exact path="/" render={() => <Posts posts={posts} />} />
+          <Route path="/create" render={() => <CreatePost />} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 }
 
