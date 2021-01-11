ce0bbda8cbb5a3c9cac3c1dd8ce6ccfd9f2a8c42
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 15:30:42 +0100

    added labels and inputs

--- blog-app/src/components/AddPost.js
+++ blog-app/src/components/AddPost.js
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Button, Input, InputLabel } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+// import theme from '../theme';
 
 const useStyles = makeStyles({
+  addForm: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputStyle: {
+    marginBottom: '2rem',
+    paddingLeft: '0.3rem',
+  },
+  labelStyle: {
+    marginBottom: '0.5rem',
+  },
   buttonStyle: {
-    color: 'green',
+    background: '#333',
+    color: '#fff',
   }
 });
 
 const AddPost = () => {
 
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   const classes = useStyles();
 
   return (
-    <div>
-      <Button  className={classes.buttonStyle} variant="outlined" color="inherit">Add</Button>
-    </div>
+    <form className={classes.addForm}>
+      <div>
+        <InputLabel className={classes.labelStyle} htmlFor="title" margin="normal">Title</InputLabel>
+        <Input className={classes.inputStyle} id="title" value={title} onChange={e => setTitle(e.target.value)} fullWidth />
+      </div>
+      <div>
+        <InputLabel  className={classes.labelStyle}  htmlFor="title" >Body</InputLabel>
+        <Input className={classes.inputStyle} id="body" value={body} onChange={e => setBody(e.target.value)} fullWidth />
+      </div>
+      <div>
+        <Button className={classes.buttonStyle}>Add</Button>
+      </div>
+    </form>
   )
 }
 
