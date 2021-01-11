11d3ac9bbc06784843d7cb75bfcc0954621e1d1c
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 17:39:46 +0100

    added createpost as a prop in addpost component

--- blog-app/src/views/CreatePost.js
+++ blog-app/src/views/CreatePost.js
@@ -2,11 +2,11 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 import AddPost from '../components/AddPost'
 
-const CreatePost = () => {
+const CreatePost = ({createPost}) => {
   return (
     <div>
       <Typography variant="h4" gutterBottom>Create Post</Typography>
-      <AddPost />
+      <AddPost createPost={createPost} />
     </div>
   )
 }
