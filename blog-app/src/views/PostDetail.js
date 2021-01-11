555b5b5418aa4cda3033c185c553f7f041a7a216
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 17:40:01 +0100

    created post detail page

---
+++ blog-app/src/views/PostDetail.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Typography } from '@material-ui/core';
+
+const PostDetail = ({ match }) => {
+
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState({});
+
+  const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+  const getPost = async () => {
+    setLoading(true)
+    const res = await axios.get(url + `${match.params.id}`)
+    setPost(res.data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getPost()
+  },[])
+
+  if (!loading) {
+    return (
+      <Box>
+        <Typography variant="h5" gutterBottom align="center">
+          {post.title}
+        </Typography>
+        <Typography variant="body2">
+          {post.body}
+        </Typography>
+      </Box>
+    )
+  } else {
+    return <p>Loading...</p>
+  }
+}
+
+export default PostDetail
