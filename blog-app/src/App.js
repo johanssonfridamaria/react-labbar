dd8ab4599d4436423e257567d6929d1e00a5c361
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 17:38:00 +0100

    added route to postDetails and created function to store posts in localstorage

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -7,35 +7,55 @@ import { Container, Box } from '@material-ui/core';
 import CreatePost from './views/CreatePost';
 import Posts from './views/Posts';
 import axios from 'axios';
+import PostDetail from './views/PostDetail';
 
 function App() {
 
-  const [posts, setposts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const url = 'https://jsonplaceholder.typicode.com/posts/';
 
   const getPosts = async () => {
     const response = await axios.get(url)
-    setposts(response.data)
+    setPosts(response.data)
   }
 
   useEffect(() => {
     getPosts()
+    const storedPosts = JSON.parse(localStorage.getItem('posts'))
+    if(storedPosts){
+      setPosts(storedPosts)
+    }
   }, [])
 
+  useEffect(()=>{
+    localStorage.setItem('posts', JSON.stringify(posts))
+  }, [posts])
 
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Box mt={4} >
-        <Container fixed>
-          <Switch>
-            <Route exact path="/" render={() => <Posts posts={posts} />} />
-            <Route path="/create" render={() => <CreatePost />} />
-          </Switch>
-        </Container>
-      </Box>
-    </BrowserRouter>
-  );
+  const createPost = (title, body) => {
+    
+    let post = {
+      id: Date.now().toString(),
+      title,
+      body
+    }
+    setPosts(prevPosts => {
+      return [post, ...prevPosts]
+     })}
+
+return (
+  <BrowserRouter>
+    <Navbar />
+    <Box mt={4} >
+      <Container fixed>
+        <Switch>
+          <Route exact path="/" render={() => <Posts posts={posts} />} />
+          <Route exact path="/create" render={() => <CreatePost createPost={createPost} />} />
+          <Route exact path="/post/:id" component={PostDetail} />
+        </Switch>
+      </Container>
+    </Box>
+  </BrowserRouter>
+);
 }
 
 export default App;
