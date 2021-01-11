8ea327ca863ca5f87e6069548fb2eddf79bf571b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-11 15:38:49 +0100

    removed icon and added styling

--- blog-app/src/components/Navbar.js
+++ blog-app/src/components/Navbar.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  navbar: {
+    background: 'darkBlue'
+  },
+  logo: {
+    flex: 1,
+    color: '#fff'
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#fff',
+    marginLeft: '1rem',
+  },
+  isActive: {
+    color: '#ddd'
+  }
+
+})
+
 
 const Navbar = () => {
+
+  const classes = useStyles();
+
   return (
-    <AppBar position="static">
+    <AppBar className={classes.navbar} position="static">
       <Toolbar>
-        <IconButton >
-          <MenuIcon />
-        </IconButton>
-        <Typography >Blog</Typography>
-        <NavLink to="/">Posts</NavLink>
-        <NavLink to="/create">Add post</NavLink>
+        <Typography variant="h5" className={classes.logo} >Blog</Typography>
+        <Typography>
+          <NavLink activeClassName={classes.isActive} exact className={classes.link} to="/">Posts</NavLink>
+          <NavLink activeClassName={classes.isActive} exact className={classes.link} to="/create">Create post</NavLink>
+        </Typography>
       </Toolbar>
     </AppBar>
   )
